b9847ab705fc18f55b2f5bbf78c8bc4d54321dd8
Dhruvi-Bhayani <bhayanidhruvi0207@gmail.com>
2025-12-10 09:21:40 +0530

    Changes in db.js file in add deployment credentails.

--- server/src/db.js
+++ server/src/db.js
@@ -1,16 +1,29 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
+
+const DB_NAME = process.env.DB_NAME || "railway";
+const DB_USER = process.env.DB_USER || "root";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
+const DB_PORT = Number(process.env.DB_PORT || 3306);
+
+console.log("✅ DB CONFIG:", {
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_NAME,
+});
 
 export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT), // <-- convert to number
-    dialect: 'mysql',
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: "mysql",
     logging: false,
   }
 );
